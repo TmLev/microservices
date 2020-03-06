4d37122e098add6cf03d92c6f0fcc0aa54d9785b
TmLev <leva.bugatti@gmail.com>
2020-03-06 19:25:43 +0300

    Basic Product model

--- hw-online-store/backend/models.py
+++ hw-online-store/backend/models.py
@@ -1,3 +1,60 @@
+# coding=utf-8
+
+from __future__ import annotations
+
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.TextField(
+    )
+    category = models.TextField(
+    )
+    code = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
+
+    @staticmethod
+    def get_by_code(
+        code: uuid.UUID,
+    ) -> Product:
+        return Product.objects.get(code=code)
+
+    @staticmethod
+    def create(
+        title: str,
+        category: str,
+    ) -> uuid.UUID:
+        product: Product = Product(
+            title=title,
+            category=category,
+        )
+        product.save()
+        return product.code
+
+    @staticmethod
+    def delete_by_code(
+        code: uuid.UUID,
+    ) -> None:
+        product: Product = Product.get_by_code(code)
+        product.delete()
+
+    def to_dict(self):
+        return {
+            "title":    self.title,
+            "category": self.category,
+            "code":     self.code,
+        }
+
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
+
+    def __str__(self):
+        return self.title
