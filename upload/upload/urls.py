cd8eacd66b7711dc0414492a37a9392bae2e89a6
TmLev <leva.bugatti@gmail.com>
2020-06-13 03:33:37 +0300

    feat: adjust urls

--- upload/upload/urls.py
+++ upload/upload/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from backend.views import ProductUpload
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('api/upload', ProductUpload.as_view()),
 ]
