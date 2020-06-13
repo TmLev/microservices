cdebda0ccd8686dd4ae953f2e9c5667367e089d1
TmLev <leva.bugatti@gmail.com>
2020-06-13 07:43:46 +0300

    feat: tasks import for scheduling

--- import/import/urls.py
+++ import/import/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+# side-effect: tasks scheduling
+import backend.tasks
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
