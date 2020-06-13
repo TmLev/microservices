74b3825b325a37a2ad36a2b59dff2feb3b091da5
TmLev <leva.bugatti@gmail.com>
2020-06-13 07:43:09 +0300

    feat: copy from online-store

--- import/backend/models.py
+++ import/backend/models.py
@@ -1,3 +1,60 @@
+# coding=utf-8
+
+import hashlib
+
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    id = models.TextField(
+        primary_key=True,
+    )
+    title = models.TextField(
+    )
+    category = models.TextField(
+    )
+
+    @staticmethod
+    def get_by_id(
+        id_: str,
+    ) -> 'Product':
+        return Product.objects.get(id=id_)
+
+    @staticmethod
+    def create(
+        title: str,
+        category: str,
+        id_: str = "",
+    ) -> str:
+        hash_ = id_ or hashlib.blake2s(
+            data=(title + category).encode(),
+        ).hexdigest()
+
+        product: Product = Product(
+            id=hash_,
+            title=title,
+            category=category,
+        )
+
+        product.save()
+
+        return product.id
+
+    @staticmethod
+    def delete_by_id(
+        id_: str,
+    ) -> None:
+        product: Product = Product.get_by_id(id_)
+        product.delete()
+        return
+
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
+
+    def __str__(
+        self
+    ) -> str:
+        return self.title
